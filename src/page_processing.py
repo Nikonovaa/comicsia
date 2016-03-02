995ac7d7f716e03fc51f6c0d15f077111b0cfe1a
Anastasia Nikonova <nikonovanastya92@gmail.com>
2016-03-03 00:03:59 +0500

    Добавила основную логику с заглушками, парсинг странички готов

---
+++ src/page_processing.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+
+import os
+from lxml import html
+from urllib2 import urlopen
+
+
+def get_images_link(url, page):
+    return [] 
+
+
+def get_page_count(tree):
+    return 0
+
+
+def download(url, dest):
+    if os.path.isdir(dest):
+        os.mkdir(dest)    
+    main_url_data = urlopen(url).read()
+    tree = html.fromstring(main_url_data)
+    page_count = get_page_count(tree)
+    for page in range(1, page_count + 1):
+        for link in get_images_link(url, page):
+            save_image(link, dest)
+
+
+download_dir = os.environ.get('PWD') + '/download'
+url = 'http://comicsia.ru/collections/swine'
+download(url, download_dir)
