709cafa9cb7ca57c50e88f54de8f9f1e1e912cdd
Anastasia Nikonova <nikonovanastya92@gmail.com>
2016-03-03 14:54:03 +0500

    Теперь можно переопределять папку для загрузки

--- src/__init__.py
+++ src/__init__.py
@@ -4,7 +4,7 @@
 import os
 from page_processing import download
 
-DIR = os.environ.get('PWD') + '/download'
+DIR = os.environ.get('PWD') + '/' + os.environ.get('DST', 'download')
 MAIN_URL = os.environ.get('URL', 'http://comicsia.ru/collections/swine')
 
 
